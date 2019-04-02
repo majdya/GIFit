9b4ac97c8825a8bb0dd2eeb098b03aa0204dbd27
mrfong <therealshadix@gmail.com>
2019-04-02 15:25:29 +0300

    defined extension types

--- private/handler.js
+++ private/handler.js
@@ -1,2 +1,9 @@
 var fs = require("fs");
 var path = require("path");
+
+var exType = {
+  html: { "Content-Type": "text/html" },
+  css: { "Content-Type": "text/css" },
+  js: { "Content-Type": "application/javascript" },
+  json: { "Content-Type": "application/json" }
+};
