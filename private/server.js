5dfd10208112619282dd70d2223bc830812f0f6b
mrfong <therealshadix@gmail.com>
2019-04-02 15:22:00 +0300

    added process.env.port

--- private/server.js
+++ private/server.js
@@ -1,6 +1,7 @@
 const http = require("http");
-const router = require("./routes");
+const router = require("./router");
 const server = http.createServer(router);
-const PORT = 5000;
+const PORT = 5000 || process.env.PORT;
 
 server.listen(PORT, () => console.log("Server is listening on port %s", PORT));
+d;
