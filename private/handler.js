19ec5372621d87c508a65421dbc43a4d6f2970e6
mrfong <therealshadix@gmail.com>
2019-04-02 15:26:14 +0300

    added a handler for the main index page

--- private/handler.js
+++ private/handler.js
@@ -7,3 +7,17 @@ var exType = {
   js: { "Content-Type": "application/javascript" },
   json: { "Content-Type": "application/json" }
 };
+
+const indexHandler = res => {
+  var filePath = path.join(__dirname, "..", "public", "index.html");
+  fs.readFile(filePath, function(error, file) {
+    if (error) {
+      res.writeHead(500, exType.html);
+      res.end("<h1>sorry, something went wrong</h1>");
+    } else {
+      res.writeHead(200, exType.html);
+      res.write(file);
+    }
+    res.end();
+  });
+};
