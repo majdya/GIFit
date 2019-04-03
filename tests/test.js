650b777d8ea4b386616f3f51c02614b7a494346f
majdya <majd.ya.1995@gmail.com>
2019-04-03 12:11:15 +0300

    adding the dependencies for the tap-spec/tape/supertest

---
+++ tests/test.js
@@ -0,0 +1,15 @@
+let test = require('tape');
+// let index = require('./BL/filter.js');
+// let restList = require('./DB/restList.json');
+
+//return key of first object in array
+test('Example test', function(t) {
+  // let arr = (restList);
+  // let word = "Fatt"
+  //
+  // let actual = index.findWords(arr, word);
+  // let expected = [ {"Fattoush": "Haifa"} ]
+
+  t.equal(1, 1);
+  t.end();
+})
