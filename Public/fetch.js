9492352f3493d1f71ce6bdd42fe4951bd68343c4
mrfong <therealshadix@gmail.com>
2019-04-04 11:31:31 +0300

    added a loading cursor for when the images load relates to #29

--- Public/fetch.js
+++ Public/fetch.js
@@ -4,24 +4,23 @@ var inputField = document.getElementById("search-input");
 
 function hey(value) {
   fetch("search?" + value)
-    .then(
-      (response) => {
-        if (response.status !== 200) {
-          console.log('Looks like there was a problem. Status Code: ' +
-            response.status);
-          return;
-        }
-        response.json().then((data) => {
-          data = JSON.parse(data);
-          data = data.data.slice(0, 13)
-          for (var i = 1; i < 13; i++) {
-            document.getElementById("img" + i).src = data[i].images.downsized.url;
-          }
-        })
+    .then(response => {
+      if (response.status !== 200) {
+        console.log(
+          "Looks like there was a problem. Status Code: " + response.status
+        );
+        return;
       }
-    )
-    .catch((err) => {
-      console.log('Fetch Error :-S', err);
+      response.json().then(data => {
+        data = JSON.parse(data);
+        data = data.data.slice(0, 13);
+        for (var i = 1; i < 13; i++) {
+          document.getElementById("img" + i).src = data[i].images.downsized.url;
+        }
+      });
+    })
+    .catch(err => {
+      console.log("Fetch Error :-S", err);
     });
 }
 inputField.addEventListener("keyup", function(event) {
@@ -30,6 +29,9 @@ inputField.addEventListener("keyup", function(event) {
   if (event.keyCode === 13) {
     hey(document.getElementById("search-input").value);
     contentDiv.classList.replace("display-none", "display");
-
+    document.body.style.cursor = "wait";
+    setTimeout(function() {
+      document.body.style.cursor = "default";
+    }, 3000);
   }
 });
